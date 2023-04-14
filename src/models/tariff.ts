6a13650eed5c45e28bd758bd7275a35ec84bfc47
Ali Nabati <AliNabati93@gmail.com>
2023-04-14 15:02:10 +0330

    add required interfaces

---
+++ src/models/tariff.ts
@@ -0,0 +1,6 @@
+export interface Tariff {
+  name: string;
+  annualCostsText: string;
+  baseCostsText: string;
+  consumptionCostsText: string;
+}
---
+++ src/models/tarrif-filters.ts
@@ -0,0 +1,3 @@
+export interface TariffFilters {
+  consumption: number;
+}
